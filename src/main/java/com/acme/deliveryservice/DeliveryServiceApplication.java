1ea2a69c2f39ab53411d6e18d53a3758cd265683
Giorgos <giorgosthf@gmail.com>
2022-08-17 21:00:51 +0300

    Main Application commit

---
+++ src/main/java/com/acme/deliveryservice/DeliveryServiceApplication.java
@@ -0,0 +1,14 @@
+package com.acme.deliveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class} )
+public class DeliveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryServiceApplication.class, args);
+    }
+
+}
