7099ea2fe559ccf6f526fbf14aa334cea3c6e002
Giorgos <giorgosthf@gmail.com>
2022-08-17 21:17:07 +0300

    Created CustomerRepository with implementations

--- src/main/java/com/acme/deliveryservice/repository/CustomerRepository.java
+++ src/main/java/com/acme/deliveryservice/repository/CustomerRepository.java
@@ -2,13 +2,12 @@ package com.acme.deliveryservice.repository;
 
 import com.acme.deliveryservice.domain.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSpecificationExecutor<Customer> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 
     List<Customer> findAllByActiveIsTrue();
