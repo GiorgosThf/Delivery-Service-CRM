df1a446ca167b81ad743144e5d6ed1a4de98b9e2
Giorgos <giorgosthf@gmail.com>
2022-08-20 01:48:30 +0300

    Implemented log in system with jdbc auth def_Schema

---
+++ src/main/java/com/acme/deliveryservice/login/SecurityConfiguration.java
@@ -0,0 +1,66 @@
+package com.acme.deliveryservice.login;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import javax.sql.DataSource;
+
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Autowired
+    public DataSource dataSource;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf().disable()
+                .authorizeHttpRequests()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/home").authenticated()
+                .antMatchers("/**").hasRole("ADMIN")
+                //  .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/home")
+                //.failureUrl("/error")
+                .failureForwardUrl("/error")
+                .permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().antMatchers("/ignore1", "/ignore2");
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+
+}
